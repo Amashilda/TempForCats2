bd41731758b2d31627eb3b462b6e2e089ad276d6
Amashilda <Amashilda@gmail.com>
2015-01-05 18:01:13 +0300

    layouts

---
+++ TempForCats2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempForCats2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public RedirectToRouteResult Index()
+        {
+            return ReturnToAction("MainPost");
+        }
+
+
+        public ActionResult MainPost()
+        {
+            return View();
+        }
+
+        public ActionResult AllPosts()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ TempForCats2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TempForCats2.MvcApplication" Language="C#" %>
